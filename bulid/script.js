13866fc3ebf6469a9c31677c5b5fffd7c0af3c15
CIBI-OFF <cibiccf437@gmail.com>
2023-05-31 21:42:52 +0530

    first commit 1.0

---
+++ bulid/script.js
@@ -0,0 +1,26 @@
+function countdown() {
+  const targetDate = new Date('2023-06-10');
+  const now = new Date().getTime();
+  const distance = targetDate - now;
+
+  // Calculations for days, hours, minutes, and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Update the timer display
+  document.getElementById('days').innerText = formatTime(days);
+  document.getElementById('hours').innerText = formatTime(hours);
+  document.getElementById('minutes').innerText = formatTime(minutes);
+  document.getElementById('seconds').innerText = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+// Update the countdown every second
+setInterval(countdown, 1000);
